8cfce2b9f1ab89c27899540508e227ba9ad3f62d
Valentin Nasraty <valentin.nasraty@epitech.eu>
2019-02-03 14:38:04 +0100

    Navigation drawer en cours
    a debuguer

--- app/src/main/java/com/github/nasrat_v/maktaba_android_frontend_mvp/TabbedActivity.kt
+++ app/src/main/java/com/github/nasrat_v/maktaba_android_frontend_mvp/TabbedActivity.kt
@@ -8,6 +8,8 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
@@ -42,7 +44,6 @@ class TabbedActivity : AppCompatActivity() {
 
         container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
         tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
-
     }
 
 
