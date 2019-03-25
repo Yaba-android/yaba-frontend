b5e16b280e2e09b4a28d9a465f4faf78965b36ec
Valentin Nasraty <valentin.nasraty@epitech.eu>
2019-03-25 12:08:15 +0100

    recommended some padding because of new font

--- app/src/main/java/com/github/nasrat_v/maktaba_android_frontend_mvp/Listable/BottomOffsetDecoration.kt
+++ app/src/main/java/com/github/nasrat_v/maktaba_android_frontend_mvp/Listable/BottomOffsetDecoration.kt
@@ -6,8 +6,7 @@ import android.support.v7.widget.RecyclerView
 import android.support.annotation.DimenRes
 import android.view.View
 
-class BottomOffsetDecoration(private val mItemOffset: Int)
-    : RecyclerView.ItemDecoration() {
+class BottomOffsetDecoration(private val mItemOffset: Int) : RecyclerView.ItemDecoration() {
 
     constructor(context: Context, @DimenRes itemOffsetId: Int)
             : this(context.resources.getDimensionPixelSize(itemOffsetId))
@@ -15,8 +14,6 @@ class BottomOffsetDecoration(private val mItemOffset: Int)
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
         super.getItemOffsets(outRect, view, parent, state)
 
-        if (parent.getChildAdapterPosition(view) != (parent.adapter!!.itemCount - 1)) { // not last item
-            outRect.set(0, 0, 0, mItemOffset)
-        }
+        outRect.set(0, 0, 0, mItemOffset)
     }
 }
