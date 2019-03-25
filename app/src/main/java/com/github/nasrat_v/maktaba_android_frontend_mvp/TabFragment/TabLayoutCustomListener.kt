57b818a6e39a01a14a5d384a294fb0a576d96cad
Valentin Nasraty <valentin.nasraty@epitech.eu>
2019-03-25 13:17:44 +0100

    setting notoSansFont for all views

--- app/src/main/java/com/github/nasrat_v/maktaba_android_frontend_mvp/TabFragment/TabLayoutCustomListener.kt
+++ app/src/main/java/com/github/nasrat_v/maktaba_android_frontend_mvp/TabFragment/TabLayoutCustomListener.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import android.graphics.Typeface
 import android.support.design.widget.TabLayout
 import android.support.v4.content.ContextCompat
+import android.support.v4.content.res.ResourcesCompat
+import android.util.TypedValue
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.github.nasrat_v.maktaba_android_frontend_mvp.R
+import org.readium.r2.streamer.server.Ressources
 
 class TabLayoutCustomListener(var context: Context) {
 
@@ -30,16 +33,20 @@ class TabLayoutCustomListener(var context: Context) {
     fun setTabTextToBold(tabLayout: TabLayout, indexTab: Int) {
         val linearLayout = (tabLayout.getChildAt(0) as ViewGroup).getChildAt(indexTab) as LinearLayout
         val tabTextView = linearLayout.getChildAt(1) as TextView
+        val typeface = ResourcesCompat.getFont(context, R.font.noto_sans_family)
 
-        tabTextView.setTypeface(tabTextView.typeface, Typeface.BOLD)
-        tabTextView.setTextColor(ContextCompat.getColor(context, R.color.colorTextBlack))
+        tabTextView.setTypeface(typeface, Typeface.BOLD)
+        tabTextView.setTextColor(ContextCompat.getColor(context, R.color.colorReviewTextBlack))
+        tabTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14f)
     }
 
     fun setTabTextToNormal(tabLayout: TabLayout, indexTab: Int) {
         val linearLayout = (tabLayout.getChildAt(0) as ViewGroup).getChildAt(indexTab) as LinearLayout
         val tabTextView = linearLayout.getChildAt(1) as TextView
+        val typeface = ResourcesCompat.getFont(context, R.font.noto_sans_family)
 
-        tabTextView.setTypeface(null, Typeface.NORMAL)
+        tabTextView.setTypeface(typeface, Typeface.NORMAL)
         tabTextView.setTextColor(ContextCompat.getColor(context, R.color.colorTextGrey))
+        tabTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14f)
     }
 }
